153cb4b38b0ba6adef2df1f5ac17e082e84ba817
shashanktiwari <shashank09tiwari@gmail.com>
2024-10-20 17:10:44 +0530

    initial commit

---
+++ src/main/java/org/ABCCompany/Employee.java
@@ -0,0 +1,88 @@
+package org.ABCCompany;
+
+import java.util.Objects;
+
+public class Employee {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private Integer salary;
+    private Integer managerId;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String firstName, String lastName, Integer salary, Integer managerId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.managerId = managerId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Integer managerId) {
+        this.managerId = managerId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id.equals(employee.id) && Objects.equals(managerId, employee.managerId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, managerId);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                ", managerId=" + managerId +
+                '}';
+    }
+
+}
---
+++ src/test/java/org/example/CompanyDataTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CompanyDataTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
