4a50f02c1b682aa43b1e717b79154f972656fbe3
shashanktiwari <shashank09tiwari@gmail.com>
2024-10-20 17:47:30 +0530

    Class Name change

--- src/main/java/org/ABCCompany/PrintCompanyData.java
+++ src/main/java/org/ABCCompany/PrintCompanyData.java
@@ -7,15 +7,15 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.*;
 
-public class CompanyData {
+public class PrintCompanyData {
 
     private static Integer ceoId;
 
     public static void main(String[] args) {
         Map<Integer, Employee> empMap = loadCSV();
-        CompanyData companyData = new CompanyData();
-        companyData.findManagerWithLessSalary(empMap);
-        companyData.findHierarchy(empMap);
+        PrintCompanyData printCompanyData = new PrintCompanyData();
+        printCompanyData.findManagerWithLessSalary(empMap);
+        printCompanyData.findHierarchy(empMap);
         System.out.println("Hello World!");
     }
 
@@ -23,7 +23,7 @@ public class CompanyData {
     public static Map<Integer, Employee> loadCSV() {
         Map<Integer, Employee> hm = new LinkedHashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader("/Users/shashanktiwari/Downloads/OrgStructAnalysis/src/main/java/org/ABCCompany/resources/Employee.csv"))) {
-            String line = "";
+            String line ;
             br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] strArr = line.split(",");
@@ -34,13 +34,12 @@ public class CompanyData {
                     managerId = Integer.valueOf(strArr[4]);
                 } else {
                     ceoId = id;
-                    managerId = null;
                 }
                 Employee emp = new Employee(id, strArr[1], strArr[2], salary, managerId);
                 hm.put(id, emp);
             }
         } catch (IOException ex) {
-            System.out.println(" exception occurred " + ex.toString());
+            System.out.println(" exception occurred " + ex);
         }
         return hm;
     }
@@ -50,7 +49,7 @@ public class CompanyData {
         for (Map.Entry<Integer, Employee> entry : empMap.entrySet()) {
             Integer empId = entry.getKey();
             Employee emp = entry.getValue();
-            if (empId != ceoId) {
+            if (!empId.equals(ceoId)) {
                 List<Employee> directReportList = new ArrayList<>();
                 if (mngrEmpMap.containsKey(emp.getManagerId())) {
                     directReportList = mngrEmpMap.get(emp.getManagerId());
@@ -63,32 +62,28 @@ public class CompanyData {
         Map<Double, Employee> mngWithLessSal = new LinkedHashMap<>();
         Map<Double, Employee> mngWithMoreSal = new LinkedHashMap<>();
         for (Map.Entry<Integer, List<Employee>> mgMap : mngrEmpMap.entrySet()) {
-            Double avgSal = mgMap.getValue().stream().map(e -> e.getSalary()).mapToDouble(Integer::doubleValue).average().getAsDouble();
+            double avgSal = mgMap.getValue().stream().map(Employee::getSalary).mapToDouble(Integer::doubleValue).average().getAsDouble();
             Integer mngId = mgMap.getKey();
             Double mngSal = Double.valueOf(empMap.get(mngId).getSalary());
-            Double per = (((mngSal - avgSal) / avgSal) * 100);
+            Double diffPer = (((mngSal - avgSal) / avgSal) * 100);
             final DecimalFormat df = new DecimalFormat("0.00");
             df.setRoundingMode(RoundingMode.UP);
 
-            Double diff = Double.valueOf(df.format(per));
+            double diff = Double.valueOf(df.format(diffPer));
             if (diff <= 20) {
                 mngWithLessSal.put(diff, empMap.get(mngId));
             }
             if (diff > 50) {
                 mngWithMoreSal.put(diff, empMap.get(mngId));
             }
-            System.out.println("per ----> " + per + " Employee Id: " + mngId + " average sal :" + avgSal + "  mngSal:  " + mngSal);
+            System.out.println("per ----> " + diffPer + " Employee Id: " + mngId + " average sal :" + avgSal + "  mngSal:  " + mngSal);
 
         }
         System.out.println("Below are the Employee ID and name of manager's who earns less than 20% of average salary of their subordinates and by how much respectively");
-        mngWithLessSal.entrySet().stream().forEach(hm -> {
-            System.out.println("Employee Id: " + hm.getValue().getId() + " and Name: " + hm.getValue().getFirstName() + " " + hm.getValue().getLastName() + " also the difference in salary is by " + hm.getKey() + "%");
-        });
+        mngWithLessSal.entrySet().forEach(hm -> System.out.println("Employee Id: " + hm.getValue().getId() + " and Name: " + hm.getValue().getFirstName() + " " + hm.getValue().getLastName() + " also the difference in salary is by " + hm.getKey() + "%"));
         System.out.println(" *********** ");
         System.out.println("Below are the Employee ID and name of manager's who earns more than 50% of average salary of their subordinates and by how much respectively");
-        mngWithMoreSal.entrySet().stream().forEach(hm -> {
-            System.out.println("Employee Id: " + hm.getValue().getId() + " and Name: " + hm.getValue().getFirstName() + " " + hm.getValue().getLastName() + " also the difference in salary is by " + hm.getKey() + "%");
-        });
+        mngWithMoreSal.entrySet().forEach(hm -> System.out.println("Employee Id: " + hm.getValue().getId() + " and Name: " + hm.getValue().getFirstName() + " " + hm.getValue().getLastName() + " also the difference in salary is by " + hm.getKey() + "%"));
 
     }
 
--- src/test/java/org/ABCCompany/PrintCompanyDataTest.java
+++ src/test/java/org/ABCCompany/PrintCompanyDataTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 /**
  * Unit test for simple App.
  */
-public class CompanyDataTest {
+public class PrintCompanyDataTest {
 
     /**
      * Rigorous Test :-)
